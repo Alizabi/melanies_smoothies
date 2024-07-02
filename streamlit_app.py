147b90789bb119d21e95750bdb0002521aab1f83
Alizabi <alexandra-heiniger@gmx.ch>
2024-07-02 18:07:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,6 @@ session = cnx.session()
 ##session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.dataframe(daata=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
